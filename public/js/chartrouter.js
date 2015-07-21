307c47701e688b72876c948c6dec5651ef1d95c0
vingorius <vingorius@gmail.com>
2015-07-21 17:53:37 +0900

    add loading screen

--- public/js/chartrouter.js
+++ public/js/chartrouter.js
@@ -39,45 +39,16 @@ var CHART = {
 define("router", [CHART.INIT, "utils"], function(TARGET, _utils)	{
 	var chart_set = { url : CHART.URL, func : TARGET };
 
-	var create_xmlhttprequest = function()	{
-		return (window.XMLHttpRequest) 
-		? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-	}
-
-	var request_xhr = function()	{
-		var xhr = create_xmlhttprequest();
-		// $(".chart_base_container").hide();
-		xhr.open("GET", chart_set.url, true);
-		xhr.onreadystatechange = function(_event)	{
-			if(xhr.readyState === 4 && xhr.status === 200)	{
-				var res = JSON.parse(xhr.responseText);
-				chart_set.func(res);
-			}
-			else { 
-				return; 
-			}
-		}
-		xhr.onload = function(_event)	{
-			console.log("onload", _event)
-			setTimeout(function() {
-				$(".chart_base_container").fadeIn();
-				$("#loading_area").fadeOut();
-			}, 2000);
-		}
-		xhr.onerror = function(_event)	{
-			console.log("onerror", _event)
-		}
-		xhr.send(null);
-	}
-
 	var request_data = function()    {    
-		$.get(chart_set.url, function(_res)   { 
-			chart_set.func(_res); 
+		$.get(chart_set.url)
+		.done(function(_res)   { 
+			chart_set.func(_res);
+			$(".chart_base_container").fadeIn();
+			$("#loading_area").fadeOut(); 
 		});
 	}
 
 	return function()	{
-		// request_data();
-		request_xhr();
+		request_data();
 	}
 });
